5f2b001aedb1dd8d32bd318f601de43a566936cf
Dao Lam <daolam112@gmail.com>
2016-09-08 16:50:59 -0700

    Add endpoint for getting pipeline's secrets

---
+++ plugins/pipelines/listSecrets.js
@@ -0,0 +1,51 @@
+'use strict';
+const boom = require('boom');
+const joi = require('joi');
+const schema = require('screwdriver-data-schema');
+const listSchema = joi.array().items(schema.models.secret.get).label('List of secrets');
+
+module.exports = () => ({
+    method: 'GET',
+    path: '/pipelines/{id}/secrets',
+    config: {
+        description: 'Get all secrets secrets for a given pipelines',
+        notes: 'Returns all secrets for a given pipeline',
+        tags: ['api', 'pipelines', 'secrets'],
+        auth: {
+            strategies: ['token', 'session'],
+            scope: ['user']
+        },
+        handler: (request, reply) => {
+            const pipelineFactory = request.server.app.pipelineFactory;
+            const credentials = request.auth.credentials;
+            const canAccess = request.server.plugins.secrets.canAccess;
+
+            return pipelineFactory.get(request.params.id)
+                .then(pipeline => {
+                    if (!pipeline) {
+                        throw boom.notFound('Pipeline does not exist');
+                    }
+
+                    return pipeline.secrets;
+                })
+                .then(secrets => {
+                    if (secrets.length === 0) {
+                        return reply([]);
+                    }
+
+                    return canAccess(credentials, secrets[0])
+                        .then(() => reply(secrets.map(s => {
+                            const output = s.toJson();
+
+                            delete output.value;
+
+                            return output;
+                        })));
+                })
+                .catch(err => reply(boom.wrap(err)));
+        },
+        response: {
+            schema: listSchema
+        }
+    }
+});
