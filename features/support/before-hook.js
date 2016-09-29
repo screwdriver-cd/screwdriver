399fa216f2f5a2dce7c9fae7e6511ef39b26f8da
Dao Lam <daolam112@gmail.com>
2016-09-29 15:48:23 -0700

    before-hook

--- features/support/before-hook.js
+++ features/support/before-hook.js
@@ -1,5 +1,18 @@
 'use strict';
 const config = require('../../.func_config');
+const requestretry = require('requestretry');
+
+/**
+ * Retry until the build has finished
+ * @method retryStrategy
+ * @param  {Object}      err
+ * @param  {Object}      response
+ * @param  {Object}      body
+ * @return {Boolean}     Retry the build or not
+ */
+function buildRetryStrategy(err, response, body) {
+    return err || body.status === 'QUEUED' || body.status === 'RUNNING';
+}
 
 /**
  * Before hooks
@@ -11,6 +24,18 @@ function beforeHooks() {
         this.username = config.username;
         this.github_token = config.github_token;
         this.jwt = config.jwt;
+        this.accessKey = process.env.ACCESS_KEY;
+        this.instance = 'https://api.screwdriver.cd';
+        this.namespace = 'v4';
+        this.waitForBuild = (buildID) =>
+            requestretry({
+                uri: `${this.instance}/${this.namespace}/builds/${buildID}`,
+                method: 'GET',
+                maxAttempts: 10,
+                retryDelay: 5000,
+                retryStrategy: buildRetryStrategy,
+                json: true
+            });
         cb();
     });
 }
