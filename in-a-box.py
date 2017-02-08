86727dba8ad21e456bddb1ce9f05df6d1c45f609
Dwight Hubbard <dhubbard@yahoo-inc.com>
2017-02-07 17:26:36 -0800

    Fix a PEP8 style issues and PyLint issues
    Use getpass for credential input

--- in-a-box.py
+++ in-a-box.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python
 # coding=utf-8
+"""
+Set up a local instance of screwdriver on the local system
+"""
 from __future__ import print_function
+import getpass
 import os
 import socket
 import sys
 import distutils.spawn
 from string import Template
-from subprocess import check_output, call, STDOUT, PIPE
+from subprocess import check_output, call, STDOUT
 try:
     from urllib.parse import urlparse
 except ImportError:
@@ -16,7 +21,7 @@ if 'raw_input' not in dir():
     raw_input = input
 
 
-dockerTemplate = '''
+DOCKER_TEMPLATE = '''
 version: '2'
 services:
     api:
@@ -74,34 +79,55 @@ def get_ip_address():
     if os.environ.get('DOCKER_HOST'):
         url = urlparse(os.environ['DOCKER_HOST'])
         return url.hostname
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(("8.8.8.8", 80))
+    return sock.getsockname()[0]
 
 
 def pad_lines(lines, length):
     """
     Left pad set of lines with spaces
     """
-    return '\n'.join(map((lambda row: ''.rjust(length, ' ') + row), lines.split('\n')))
+    return '\n'.join(
+        map((lambda row: ''.rjust(length, ' ') + row), lines.split('\n'))
+    )
 
 
-# Generate a new JWT
 def generate_jwt():
-    junk = check_output(['openssl', 'genrsa', '-out', 'jwt.pem', '1024'], stderr=STDOUT)
-    junk = check_output(['openssl', 'rsa', '-in', 'jwt.pem', '-pubout', '-out', 'jwt.pub'], stderr=STDOUT)
-    jwtPrivate = open('jwt.pem', 'r').read()
-    jwtPublic = open('jwt.pub', 'r').read()
-    junk = check_output(['rm', 'jwt.pem', 'jwt.pub'], stderr=STDOUT)
+    """
+    Generate a new JWT
+
+    Returns
+    -------
+    dict
+        Dictionary with public_key and private_key
+    """
+    check_output(
+        ['openssl', 'genrsa', '-out', 'jwt.pem', '1024'], stderr=STDOUT
+    )
+    check_output(
+        ['openssl', 'rsa', '-in', 'jwt.pem', '-pubout', '-out', 'jwt.pub'],
+        stderr=STDOUT
+    )
+    jwt_private = open('jwt.pem', 'r').read()
+    jwt_public = open('jwt.pub', 'r').read()
+    check_output(['rm', 'jwt.pem', 'jwt.pub'], stderr=STDOUT)
 
     return {
-        'public_key': pad_lines(jwtPublic, 16),
-        'private_key': pad_lines(jwtPrivate, 16)
+        'public_key': pad_lines(jwt_public, 16),
+        'private_key': pad_lines(jwt_private, 16)
     }
 
 
-# Generate OAuth credentials from GitHub.com
 def generate_oauth(ip):
+    """
+    Generate OAuth credentials from GitHub.com
+
+    Parameters
+    ----------
+    ip: str
+        The IP address
+    """
     print(Template('''
     Please create a new OAuth application on GitHub.com
     Go to https://github.com/settings/applications/new to start the process
@@ -110,23 +136,29 @@ def generate_oauth(ip):
     When done, please provide the following values:
     ''').substitute(ip=ip))
 
-    id = raw_input('    Client ID: ');
-    secret = raw_input('    Client Secret: ');
+    client_id = raw_input('    Client ID: ')
+    secret = getpass.getpass('    Client Secret: ')
 
     print('')
-    return {
-        'oauth_id': id,
-        'oauth_secret': secret
-    }
+    return dict(oauth_id=client_id, oauth_secret=secret)
 
 
 def check_component(component):
-    if distutils.spawn.find_executable(component) == None:
-        print('💀   Could not find {0}, please install and set path to {0}'.format(component))
+    """
+    Search for a component executable and exit if not found
+    """
+    if distutils.spawn.find_executable(component) is None:
+        print(
+            '💀   Could not find {0}, please install and set path to '
+            '{0}'.format(component)
+        )
         sys.exit(1)
 
 
 def main():
+    """
+    Main code function
+    """
     fields = {
         'ip': get_ip_address()
     }
@@ -144,7 +176,7 @@ def main():
     fields = dict(fields, **generate_oauth(fields['ip']))
 
     print('💾   Writing Docker Compose file')
-    compose = Template(dockerTemplate).substitute(fields)
+    compose = Template(DOCKER_TEMPLATE).substitute(fields)
     open('docker-compose.yml', 'w').write(compose)
 
     print('🚀   Screwdriver is ready to launch!')
@@ -158,9 +190,9 @@ def main():
     )
     prompt = raw_input('    Would you like to run them now? (y/n) ')
     if prompt.lower() == 'y':
-        call('docker-compose pull', shell=True)
-        call('docker-compose -p screwdriver up -d', shell=True)
-        call(Template('open http://${ip}:9000').safe_substitute(fields), shell=True)
+        call(['docker-compose', 'pull'])
+        call(['docker-compose', '-p', 'screwdriver', 'up', '-d'])
+        call(['open', Template('http://${ip}:9000').safe_substitute(fields)])
         print('\n👍   Launched!')
     else:
         print('\n👍   Skipping launch (for now)')
