8192e9a8f21eed78b3653a5836ef8df545126713
Kenta Kawaharada <kkawahar@yahoo-corp.jp>
2017-03-18 11:47:52 +0900

    Modify not to use `in`

--- features/step_definitions/secrets.js
+++ features/step_definitions/secrets.js
@@ -79,8 +79,16 @@ module.exports = function server() {
         }).then((response) => {
             this.jobId = response.body[0].id;
             this.secondJobId = response.body[1].id;
-            if (2 in response.body) this.thirdJobId = response.body[2].id;
-            this.lastJobId = response.body[response.body.length - 1].id;
+            this.thirdJobId = typeof response.body[2] === 'object' ? response.body[2].id : null;
+            this.lastJobId = (function getLastJobId() {
+                for (let i = response.body.length - 1; i >= 0; i -= 1) {
+                    if (typeof response.body[i] === 'object') {
+                        return response.body[i].id;
+                    }
+                }
+
+                return null;
+            }(response));
 
             Assert.equal(response.statusCode, 200);
         })
