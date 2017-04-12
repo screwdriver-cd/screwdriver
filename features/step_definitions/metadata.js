377d24e3ce75804b6c7f0e652387eca5b79af23f
kkawahar <xi.reich8@gmail.com>
2017-04-12 14:33:00 +0900

    feat: Fix not to capture unused string (#508)
    * Fix not to capture unused string
    
    * remove assert that does not work
    
    * Modify to later chef the value of the meta
    
    * Remove Assert.ok in metadata cucumber

--- features/step_definitions/metadata.js
+++ features/step_definitions/metadata.js
@@ -33,31 +33,23 @@ module.exports = function server() {
             throw new Error('jobName is neither BAR or BAZ');
         }
 
-        request({
+        return request({
             uri: `${this.instance}/${this.namespace}/jobs/${jobId}/builds`,
             method: 'GET',
             json: true
         }).then((response) => {
             this.buildId = response.body[0].id;
-            this.meta = response.body.meta;
 
             Assert.equal(response.statusCode, 200);
         });
     });
 
-    this.Then(/^add the { "foo": (.*) } to metadata in the "main" build container$/, () => null);
-
-    this.Then(/^add the { "bar": (.*) } to metadata in the "BAR" build container$/, () => null);
-
-    this.Then(/^in the build, the { "foo": "(.*)" } is available from metadata$/, (value) => {
-        Assert.ok('foo', this.meta);
-        Assert.equal(value, this.meta.foo);
+    this.Then(/^add the { "(.*)": "(.*)" } to metadata/, (key, value) => {
+        this.expectedMeta = this.expectedMeta || {};
+        this.expectedMeta[key] = value;
     });
 
-    this.Then(/^in the build, the { "bar": "(.*)" } is available from metadata$/, (value) => {
-        Assert.ok('bar', this.meta);
-        Assert.equal(value, this.meta.bar);
-    });
+    this.Then(/^in the build, the { "(?:.*)": "(?:.*)" } is available from metadata$/, () => null);
 
     this.Then(/^the build succeeded$/, { timeout: TIMEOUT }, () =>
         this.waitForBuild(this.buildId).then((resp) => {
@@ -73,6 +65,7 @@ module.exports = function server() {
             json: true
         }).then((response) => {
             this.buildId = response.body[0].id;
+            this.meta = response.body[0].meta;
 
             return this.waitForBuild(this.buildId).then((resp) => {
                 Assert.equal(resp.statusCode, 200);
@@ -81,9 +74,8 @@ module.exports = function server() {
     );
 
     this.Then(/^a record of the metadata is stored$/, { timeout: TIMEOUT }, () => {
-        Assert.ok('foo', this.meta);
-        Assert.ok('bar', this.meta);
-        Assert.equal('foobar', this.meta.foo);
-        Assert.equal('barbaz', this.meta.bar);
+        Object.keys(this.expectedMeta).forEach((key) => {
+            Assert.equal(this.meta[key], this.expectedMeta[key]);
+        });
     });
 };
