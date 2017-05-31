d2484c87d8741216e5e6e755b47825e9dbc8ab84
Ian Fox <ianfox97@gmail.com>
2017-05-31 11:26:35 -0700

    feat(tokens): Add token UPDATE route

---
+++ plugins/tokens/update.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const boom = require('boom');
+const joi = require('joi');
+const schema = require('screwdriver-data-schema');
+const idSchema = joi.reach(schema.models.token.base, 'id');
+
+module.exports = () => ({
+    method: 'PATCH',
+    path: '/tokens/{id}',
+    config: {
+        description: 'Update a token',
+        notes: 'Update a specific tooken',
+        tags: ['api', 'secrets'],
+        auth: {
+            strategies: ['token', 'session'],
+            scope: ['user']
+        },
+        plugins: {
+            'hapi-swagger': {
+                security: [{ token: [] }]
+            }
+        },
+        handler: (request, reply) => {
+            const tokenFactory = request.server.app.tokenFactory;
+            const userFactory = request.server.app.userFactory;
+            const username = request.auth.credentials.username;
+
+            return Promise.all([
+                tokenFactory.get(request.params.id),
+                userFactory.get({ username })
+            ])
+                .then(([token, user]) => {
+                    if (!token) {
+                        throw boom.notFound('Token does not exist');
+                    }
+
+                    if (!user) {
+                        throw boom.notFound('User does not exist');
+                    }
+
+                    if (token.userId !== user.id) {
+                        throw boom.forbidden('User does not own token');
+                    }
+
+                    return user.tokens
+                        .then((tokens) => {
+                            // Make sure it won't cause a name conflict
+                            const match = tokens && tokens.find(
+                                t => t.name === request.payload.name);
+
+                            if (match && request.params.id !== match.id) {
+                                throw boom.conflict(`Token with name ${match.name} already exists`);
+                            }
+
+                            Object.keys(request.payload).forEach((key) => {
+                                token[key] = request.payload[key];
+                            });
+
+                            return token.update()
+                            .then(() => {
+                                const output = token.toJson();
+
+                                delete output.hash;
+
+                                return reply(output).code(200);
+                            });
+                        });
+                })
+                .catch(err => reply(boom.wrap(err)));
+        },
+        validate: {
+            params: {
+                id: idSchema
+            },
+            payload: schema.models.token.update
+        }
+    }
+});
