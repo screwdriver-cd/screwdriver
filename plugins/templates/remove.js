df566c9104939f42048faa2f70475fbe2c352f97
Philip Scott <pscott@zeptohost.com>
2018-03-26 16:47:42 -0700

    Add DELETE /templates/{name} handler

---
+++ plugins/templates/remove.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const boom = require('boom');
+const joi = require('joi');
+const schema = require('screwdriver-data-schema');
+const baseSchema = schema.models.template.base;
+
+module.exports = () => ({
+    method: 'DELETE',
+    path: '/templates/{name}/',
+    config: {
+        description: 'Delete a template',
+        notes: 'Returns null if successful',
+        tags: ['api', 'templates'],
+        auth: {
+            strategies: ['token'],
+            scope: ['user', '!guest']
+        },
+        plugins: {
+            'hapi-swagger': {
+                security: [{ token: [] }]
+            }
+        },
+        handler: (request, reply) => {
+            const { name } = request.params;
+            const pipelineFactory = request.server.app.pipelineFactory;
+            const scmContext = request.auth.credentials.scmContext;
+            const templateFactory = request.server.app.templateFactory;
+            const templateTagFactory = request.server.app.templateTagFactory;
+            const userFactory = request.server.app.userFactory;
+            const username = request.auth.credentials.username;
+
+            return Promise.all([
+                templateFactory.list({ params: { name } }),
+                templateTagFactory.list({ params: { name } })
+            ]).then(([templates, tags]) => {
+                if (templates.length === 0) {
+                    throw boom.notFound('Template does not exist');
+                }
+
+                return Promise.all([
+                    userFactory.get({ username, scmContext }),
+                    pipelineFactory.get(templates[0].pipelineId)
+                ]).then(([user, pipeline]) => {
+                    if (!user) {
+                        throw boom.notFound(`User ${username} does not exist`);
+                    }
+
+                    return user.getPermissions(pipeline.scmUri);
+                })
+                    .then((permissions) => {
+                        if (!permissions.admin) {
+                            throw boom.unauthorized(`User ${username} `
+                                + 'does not have admin permission for this repo');
+                        }
+                    })
+                    .then(() => {
+                        templates.forEach(template => template.remove());
+                        tags.forEach(tag => tag.remove());
+                    })
+                    .then(() => reply().code(204));
+            })
+                .catch(err => reply(boom.wrap(err)));
+        },
+        validate: {
+            params: {
+                name: joi.reach(baseSchema, 'name')
+            }
+        }
+    }
+});
