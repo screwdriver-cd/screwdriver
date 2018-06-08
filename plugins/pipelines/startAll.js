40d2d88ad3c06407ffc808572127ec9018e618a4
Min Zhang <minzhangcmu@gmail.com>
2018-06-08 10:14:21 -0700

    fix(937): add start all child pipelines endpoint (#1109)

---
+++ plugins/pipelines/startAll.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const boom = require('boom');
+const joi = require('joi');
+const schema = require('screwdriver-data-schema');
+const idSchema = joi.reach(schema.models.pipeline.base, 'id');
+
+module.exports = () => ({
+    method: 'POST',
+    path: '/pipelines/{id}/startall',
+    config: {
+        description: 'Start all child pipelines given a specific pipeline',
+        notes: 'Start all child pipelines given a specific pipeline',
+        tags: ['api', 'pipelines'],
+        auth: {
+            strategies: ['token'],
+            scope: ['user', '!guest']
+        },
+        plugins: {
+            'hapi-swagger': {
+                security: [{ token: [] }]
+            }
+        },
+        handler: (request, reply) => {
+            const { pipelineFactory, eventFactory, userFactory } = request.server.app;
+            const { username, scmContext } = request.auth.credentials;
+            const id = request.params.id;
+            const scm = pipelineFactory.scm;
+
+            return Promise.all([
+                pipelineFactory.get(id),
+                userFactory.get({ username, scmContext })
+            ])
+                .then(([pipeline, user]) => {
+                    if (!pipeline) {
+                        throw boom.notFound('Pipeline does not exist');
+                    }
+
+                    return user.getPermissions(pipeline.scmUri)
+                        // check if user has push access
+                        .then((permissions) => {
+                            if (!permissions.push) {
+                                throw boom.unauthorized(`User ${username} `
+                                    + 'does not have push permission for this repo');
+                            }
+                        });
+                })
+                .then(() => pipelineFactory.list({
+                    params: {
+                        configPipelineId: id
+                    }
+                }))
+                .then(pipelines => pipelines.map(p =>
+                    p.token.then(token => scm.getCommitSha({
+                        scmContext,
+                        scmUri: p.scmUri,
+                        token
+                    }))
+                        .then(sha => eventFactory.create({
+                            pipelineId: p.id,
+                            sha,
+                            username,
+                            scmContext,
+                            startFrom: '~commit',
+                            causeMessage: `Started by ${username}`
+                        }))))
+                .then(() => reply().code(201))
+                .catch(err => reply(boom.wrap(err)));
+        },
+        validate: {
+            params: {
+                id: idSchema
+            }
+        }
+    }
+});
