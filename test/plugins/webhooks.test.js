cb8356555b3275d9ee24e33bc13627a5ba393132
hitakats <hitakats@yahoo-corp.jp>
2019-03-11 17:58:55 +0900

    no wait in test

--- test/plugins/webhooks.test.js
+++ test/plugins/webhooks.test.js
@@ -162,9 +162,9 @@ describe('webhooks plugin test', () => {
             create: sinon.stub()
         };
 
-        /* eslint-disable global-require */
-        plugin = require('../../plugins/webhooks');
-        /* eslint-enable global-require */
+        plugin = rewire('../../plugins/webhooks');
+        // eslint-disable-next-line no-underscore-dangle
+        plugin.__set__('WAIT_FOR_CHANGEDFILES', 0);
 
         server = new hapi.Server();
         server.root.app = {
