ee14ca2b5b9c11153403524e1f946379a75a77c2
pritamstyz4ever <pritam.paul@verizonmedia.com>
2019-11-04 11:46:36 -0500

    feat(1610): Add shutdown plugin tests and fix linting issues

--- plugins/shutdown.js
+++ plugins/shutdown.js
@@ -1,12 +1,43 @@
 'use strict';
 
-const Joi = require("joi");
+const Joi = require('joi');
 const tasks = {};
 const taskSchema = Joi.object({
     taskname: Joi.string().required(),
     task: Joi.func().required(),
     timeout: Joi.number().integer()
 });
+
+/**
+ *
+ * @param {object} task
+ */
+function register(task) {
+    const res = taskSchema.validate(task);
+
+    if (res.error) {
+        return res.error;
+    }
+    tasks[task.taskname] = task;
+
+    return '';
+}
+
+/**
+ *
+ * @param {function} fn
+ * @param {string} timeout
+ */
+function promiseTimeout(fn, timeout) {
+    return new Promise(((resolve, reject) => {
+        fn(resolve, reject);
+
+        // Set up the timeout
+        setTimeout(() => {
+            resolve(`Promise timed out after ${timeout} ms`);
+        }, timeout);
+    }));
+}
 
 /**
  * Hapi interface for plugin to handle serve graceful shutdown
@@ -17,70 +48,52 @@ const taskSchema = Joi.object({
 exports.register = (server, options, next) => {
     const taskHandler = async (resolve, reject) => {
         try {
-            await Promise.all(Object.keys(tasks).map(async key => {
-                server.log(["shutdown"], `executing task ${key}`, new Date().toISOString())
-                let item = tasks[key]
+            await Promise.all(Object.keys(tasks).map(async (key) => {
+                server.log(['shutdown'], `executing task ${key}`, new Date().toISOString());
+                const item = tasks[key];
+
                 await item.task();
-            }))
-            resolve()
+            }));
+            resolve();
+        } catch (err) {
+            console.log(err);
+            reject(err);
         }
-        catch (err) {
-            console.log(err)
-            reject(err)
-        }
-    }
+    };
     const gracefulStop = async () => {
         try {
-            server.log(["shutdown"], `gracefully shutting down server`, new Date().toISOString());
+            server.log(['shutdown'], 'gracefully shutting down server', new Date().toISOString());
             await server.root.stop({
                 timeout: 5000
             });
             process.exit(0);
-        }
-        catch (err) {
-            console.log(err)
+        } catch (err) {
+            console.log(err);
             process.exit(1);
         }
-    }
+    };
 
     const onSigterm = async () => {
         try {
-            server.log(["shutdown"], "got SIGTERM; running triggers before shutdown", new Date().toISOString());
-            let res = await promiseTimeout(taskHandler, options.terminationGracePeriod * 1000)
-            if(res) {
-                server.log(["shutdown"], res, new Date().toISOString());
-            }
-            await gracefulStop()
-        }
-        catch (err) {
-            console.log(err)
-        }
-    }
+            server.log(['shutdown'],
+                'got SIGTERM; running triggers before shutdown',
+                new Date().toISOString());
+            const res = await promiseTimeout(taskHandler, options.terminationGracePeriod * 1000);
 
-    process.on('SIGTERM', onSigterm)
+            if (res) {
+                server.log(['shutdown'], res, new Date().toISOString());
+            }
+            await gracefulStop();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    process.on('SIGTERM', onSigterm);
     server.expose('handler', register);
-    next()
-}
+    next();
+};
 
 exports.register.attributes = {
     name: 'shutdown'
 };
-
-function register(task) {
-    let res = taskSchema.validate(task);
-    if (res.error) {
-        return res.error;
-    }
-    tasks[task.taskname] = task;
-}
-
-function promiseTimeout(fn, timeout) {
-    return new Promise(function (resolve, reject) {
-        fn(resolve, reject);
-
-        // Set up the timeout
-        setTimeout(function () {
-            resolve('Promise timed out after ' + timeout + ' ms');
-        }, timeout);
-    });
-}
