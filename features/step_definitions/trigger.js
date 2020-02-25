6b4911b259d3a053c9bfbfda68fb9584fbff8747
Tiffany Kyi <tiffanykyi@gmail.com>
2020-02-24 17:32:16 -0800

    feat: Add working triggers feature tests

--- features/step_definitions/trigger.js
+++ features/step_definitions/trigger.js
@@ -3,155 +3,180 @@
 const Assert = require('chai').assert;
 const sdapi = require('../support/sdapi');
 const request = require('../support/request');
-const { defineSupportCode } = require('cucumber');
+const { Before, Given, When, Then } = require('cucumber');
 
 const TIMEOUT = 240 * 1000;
 
-defineSupportCode(({ Before, Given, When, Then }) => {
-    Before({
-        tags: '@trigger'
-    }, function hook() {
-        this.repoOrg = this.testOrg;
-        this.repoName = 'functional-trigger';
-        this.firstPipelineId = null;
-        this.secondPipelineId = null;
-        this.builds = null;
+Before({
+    tags: '@trigger'
+}, function hook() {
+    this.repoOrg = this.testOrg;
+    this.repoName = 'functional-trigger';
+    this.pipelineIdA = null;
+    this.pipelineIdB = null;
+    this.successAJobId = null;
+    this.failAJobId = null;
+    this.successBJobId = null;
+    this.failBJobId = null;
+    this.parallelAJobId = null;
+    this.parallelB1JobId = null;
+    this.parallelB2JobId = null;
+    this.builds = null;
+});
+
+Given(/^an existing pipeline on branch "(.*)" with the workflow jobs:$/, {
+    timeout: TIMEOUT
+}, function step(branch, table) {
+    let pipelineVarName = 'pipelineIdA';
+
+    if (branch === 'pipelineB') {
+        pipelineVarName = 'pipelineIdB';
+    }
+
+    return this.ensurePipelineExists({
+        repoName: this.repoName,
+        branch,
+        pipelineVarName,
+        table,
+        shouldNotDeletePipeline: true });
+});
+
+Given(/^an existing pipeline on branch "(.*)" with job "(.*)"$/, {
+    timeout: TIMEOUT
+}, function step(branch, jobName) {
+    let pipelineVarName = 'pipelineIdA';
+
+    if (branch === 'pipelineB') {
+        pipelineVarName = 'pipelineIdB';
+    }
+
+    return this.ensurePipelineExists({
+        repoName: this.repoName,
+        branch,
+        pipelineVarName,
+        jobName,
+        shouldNotDeletePipeline: true });
+});
+
+When(/^the "(.*)" job on branch "(?:.*)" is started$/,
+    { timeout: TIMEOUT }, function step(jobName) {
+        let jobId;
+        let buildVarName;
+
+        switch (jobName) {
+        case 'success_A':
+            jobId = this.successAJobId;
+            buildVarName = 'successABuildId';
+            break;
+        case 'fail_A':
+            jobId = this.failAJobId;
+            buildVarName = 'failABuildId';
+            break;
+        case 'success_B':
+            jobId = this.successBJobId;
+            buildVarName = 'successBBuildId';
+            break;
+        case 'fail_B':
+            jobId = this.failBJobId;
+            buildVarName = 'failBBuildId';
+            break;
+        case 'parallel_A':
+            jobId = this.parallelAJobId;
+            buildVarName = 'parallelABuildId';
+            break;
+        case 'parallel_B1':
+            jobId = this.parallelB1JobId;
+            buildVarName = 'parallelB1BuildId';
+            break;
+        case 'parallel_B2':
+            jobId = this.parallelB2JobId;
+            buildVarName = 'parallelB2BuildId';
+            break;
+        default: // main job
+            jobId = this.jobId;
+            buildVarName = 'buildId';
+        }
+
+        return request({
+            uri: `${this.instance}/${this.namespace}/builds`,
+            method: 'POST',
+            body: {
+                jobId
+            },
+            auth: {
+                bearer: this.jwt
+            },
+            json: true
+        }).then((resp) => {
+            Assert.equal(resp.statusCode, 201);
+
+            this[buildVarName] = resp.body.id;
+            this.buildId = resp.body.id;
+        });
     });
 
-    Given(/^two pipelines "(.*)" and "(.*)" with the following config:$/,
-        { timeout: TIMEOUT }, function step(first, second) {
-            return this.getJwt(this.apiToken)
-                .then((response) => {
-                    this.jwt = response.body.token;
+// eslint-disable-next-line no-useless-escape
+Then(/^the "(.*)" build\'s parentBuildId is that "(.*)" build\'s buildId$/, {
+    timeout: TIMEOUT
+}, function step(jobName1, jobName2) {
+    let buildVarName;
 
-                    return request({
-                        uri: `${this.baseUri}/pipelines?search=${this.repoOrg}/${this.repoName}`,
-                        method: 'GET',
-                        auth: {
-                            bearer: this.jwt
-                        },
-                        json: true
-                    });
-                })
-                .then((response) => {
-                    Assert.isArray(response);
+    switch (jobName2) {
+    case 'success_A':
+        buildVarName = 'successABuildId';
+        break;
+    case 'fail_A':
+        buildVarName = 'failABuildId';
+        break;
+    case 'success_B':
+        buildVarName = 'successBBuildId';
+        break;
+    case 'fail_B':
+        buildVarName = 'failBBuildId';
+        break;
+    case 'parallel_A':
+        buildVarName = 'parallelABuildId';
+        break;
+    case 'parallel_B1':
+        buildVarName = 'parallelB1BuildId';
+        break;
+    case 'parallel_B2':
+        buildVarName = 'parallelB2BuildId';
+        break;
+    default: // main job
+        buildVarName = 'buildId';
+    }
 
-                    const firstPipeline = response.find(pipeline =>
-                        pipeline.scmRepo.branch === first
-                    );
-                    const secondPipeline = response.find(pipeline =>
-                        pipeline.scmRepo.branch === second
-                    );
+    return sdapi.searchForBuild({
+        instance: this.instance,
+        pipelineId: this.pipelineIdB,
+        desiredStatus: ['QUEUED', 'RUNNING', 'SUCCESS', 'FAILURE'],
+        jobName: jobName1,
+        jwt: this.jwt,
+        parentBuildId: this[buildVarName]
+    })
+        .then(resultBuild => Assert.ok(resultBuild));
+});
 
-                    Assert.isOk(firstPipeline);
-                    Assert.isOk(secondPipeline);
+Then(/^the "(.*)" job on branch "(.*)" is not triggered$/, {
+    timeout: TIMEOUT
+}, function step(jobName, branch) {
+    let pipelineId = this.pipelineIdA;
 
-                    this.firstPipelineId = firstPipeline.id;
-                    this.secondPipelineId = secondPipeline.id;
-                });
-        });
+    if (branch === 'pipelineB') {
+        pipelineId = this.pipelineIdB;
+    }
 
-    When(/^the "(deploy_foo_fail|deploy_foo)" job in pipelineA is started$/,
-        { timeout: TIMEOUT }, function step(jobName) {
-            return request({
-                uri: `${this.baseUri}/events`,
-                method: 'POST',
-                body: {
-                    pipelineId: this.firstPipelineId,
-                    startFrom: jobName
-                },
-                auth: {
-                    bearer: this.jwt
-                },
-                json: true
-            }).then((resp) => {
-                Assert.equal(resp.statusCode, 201);
-                this.eventId = resp.body.id;
-            }).then(() => request({
-                uri: `${this.baseUri}/events/${this.eventId}/builds`,
-                method: 'GET',
-                auth: {
-                    bearer: this.jwt
-                },
-                json: true
-            })).then((resp) => {
-                Assert.equal(resp.statusCode, 200);
-                this.buildId = resp.body[0].id;
-            });
-        });
+    return sdapi.findBuilds({
+        instance: this.instance,
+        pipelineId,
+        jobName,
+        jwt: this.jwt
+    }).then((buildData) => {
+        let result = buildData.body || [];
 
-    When(/^the "(.*)" build failed$/, {
-        timeout: TIMEOUT
-    }, function step(jobName) {
-        return this.waitForBuild(this.buildId).then((resp) => {
-            Assert.equal(resp.statusCode, 200);
-            Assert.equal(resp.body.status, 'FAILURE', `Unexpected build status: ${jobName}`);
-        });
-    });
+        result = result.filter(item => item.sha === this.sha);
 
-    When(/^the "(.*)" build succeeded$/, {
-        timeout: TIMEOUT
-    }, function step(jobName) {
-        return this.waitForBuild(this.buildId).then((resp) => {
-            Assert.equal(resp.statusCode, 200);
-            Assert.equal(resp.body.status, 'SUCCESS', `Unexpected build status: ${jobName}`);
-        });
-    });
-
-    Then(/^the "(build_bar|build_deploy)" job in pipelineB is started$/, { timeout: TIMEOUT },
-        function step(jobName) {
-            this.jobName = jobName;
-
-            return sdapi.searchForBuild({
-                instance: this.instance,
-                pipelineId: this.secondPipelineId,
-                desiredSha: this.sha,
-                desiredStatus: ['QUEUED', 'RUNNING', 'SUCCESS', 'FAILURE'],
-                jobName: this.jobName,
-                jwt: this.jwt
-            }).then((build) => {
-                this.buildId = build.id;
-            });
-        });
-
-    Then(/^that "(build_bar|build_deploy)" build's parentBuildId is that "(.*)" build's buildId$/, {
-        timeout: TIMEOUT
-    }, function step(jobName1, jobName2) {
-        return Promise.all([
-            sdapi.searchForBuild({
-                instance: this.instance,
-                pipelineId: this.secondPipelineId,
-                desiredSha: this.sha,
-                desiredStatus: ['QUEUED', 'RUNNING', 'SUCCESS', 'FAILURE'],
-                jobName: jobName1,
-                jwt: this.jwt
-            }),
-            sdapi.searchForBuild({
-                instance: this.instance,
-                pipelineId: this.firstPipelineId,
-                desiredSha: this.sha,
-                desiredStatus: ['QUEUED', 'RUNNING', 'SUCCESS', 'FAILURE'],
-                jobName: jobName2,
-                jwt: this.jwt
-            })
-        ])
-            .then(([build1, build2]) => Assert.equal(build1.parentBuildId, build2.id));
-    });
-
-    Then(/^the "(.*)" job is not triggered$/, {
-        timeout: TIMEOUT
-    }, function step(jobName) {
-        return sdapi.findBuilds({
-            instance: this.instance,
-            pipelineId: this.secondPipelineId,
-            jobName,
-            jwt: this.jwt
-        }).then((buildData) => {
-            let result = buildData.body || [];
-
-            result = result.filter(item => item.sha === this.sha);
-
-            Assert.equal(result.length, 0, 'Unexpected job was triggered.');
-        });
+        Assert.equal(result.length, 0, 'Unexpected job was triggered.');
     });
 });
--- features/trigger.feature
+++ features/trigger.feature
@@ -1,57 +1,59 @@
+@trigger
 Feature: Remote Trigger
 
     Users should be allowed to tie together workflows from different build pipelines so that they can
     express dependencies between otherwise unrelated projects.
 
-    Eg: Pipeline A for Project Foo
-        (~commit) -> (build_foo) -> (deploy_foo)
-        (deploy_foo_fail)
-        Pipeline B for Project Bar
-        (~commit) -> (build_bar) -> (deploy_bar)
-        (build_bar_fail)
+    Eg: The following combined pipeline workflows:
+        (~commit) -> (success_A) -> (success_B)
 
-        Assume Project Bar depends on Project Foo. A user should be able to build Project Bar
-        when Project Foo has been built. A user can also make it specific to a job so that
-        Project Bar -> (build_bar) to be built when Project Foo -> (deploy_foo) is
+        (fail_A) -> (fail_B)
+
+                        (parallel_B1)
+        (parallel_A) ->
+                        (parallel_B2)
+
+
+
+        Assume Pipeline B generally depends on Pipeline A. A user should be able to build Pipeline B
+        when Pipeline A has been built. A user can also make it specific to a job so that
+        Pipeline B -> (success_B) to be built when Pipeline A -> (success_A) is
         finished successfully.
-
-        Combined Pipeline Workflows
-        (~commit) -> (build_foo) -> (deploy_foo) -> (build_bar) -> (deploy_bar)
-        (deploy_foo_fail) -> (build_bar_fail)
 
     Rules:
         - Configuration for trigger must reside in Workflow/Job being triggered
-        - Triggered workflow/job should start with latest commit on configured SCM branch.
-        - If multiple jobs in a pipeline requires the same external pipeline's Job as trigger, then
-          builds for these jobs should be part of same pipeline event.
+        - Triggered workflow/job should start with latest commit on configured SCM branch
+        //- If multiple jobs in a pipeline requires the same external pipeline's Job as trigger, then
+          builds for these jobs should be part of same pipeline event
 
-    Scenario: Builds are not triggered if required build is not successful.
-        Given two pipelines "pipelineA" and "pipelineB" with following config:
-            | job           | requires      |
-            | pipelineB:build_bar_fail | pipelineA:deploy_foo_fail |
-        When the "deploy_foo_fail" job in pipelineA is started
-        And the "deploy_foo_fail" build failed
-        Then the "build_bar_fail" job in pipelineB is not triggered
+      Scenario: External builds are not triggered if required build is not successful.
+          Given an existing pipeline on branch "pipelineA" with job "fail_A"
+          And an existing pipeline on branch "pipelineB" with the workflow jobs:
+              | job           | requires      |
+              | fail_B | ~sd@?:fail_A |
+          When the "fail_A" job on branch "pipelineA" is started
+          And the "fail_A" build failed
+          Then the "fail_B" job on branch "pipelineB" is not triggered
 
-    Scenario: Build is triggered after another build is successful.
-        Given two pipelines "pipelineA" and "pipelineB" with following config:
-            | job           | requires      |
-            | pipelineB:build_bar | pipelineA:deploy_foo |
-        When the "deploy_foo" job in pipelineA is started
-        And the "deploy_foo" build succeeded
-        Then the "build_bar" job in pipelineB is started
-        And that "build_bar" build's parentBuildId is that "deploy_foo" build's buildId
+      Scenario: External build is triggered after another build is successful.
+          Given an existing pipeline on branch "pipelineA" with job "success_A"
+          And an existing pipeline on branch "pipelineB" with the workflow jobs:
+              | job           | requires      |
+              | success_B | ~sd@?:success_A |
+          When the "success_A" job on branch "pipelineA" is started
+          And the "success_A" build succeeded
+          Then the "success_B" job on branch "pipelineB" is started
+          And the "success_B" build's parentBuildId is that "success_A" build's buildId
 
-    @ignore
-    Scenario: Fan-out. Multiple builds are triggered in parallel as a result of a build's success.
-        Given two pipelines "pipelineA" and "pipelineB" with following config:
-            | job             | requires      |
-            | pipelineB:build_bar   | pipelineA:deploy_foo |
-            | pipelineB:build_deploy | pipelineA:deploy_foo |
-        When the "deploy_foo" job in pipelineA is started
-        And the "deploy_foo" build succeeded
-        Then the build for "build_bar" job in pipelineB is started
-        And that "build_bar" build uses the same SHA as latest commit on the branch pipeline is configured
-        And the build for "build_deploy" job in pipelineB is started
-        And that "build_deploy" build uses the same SHA as latest commit on the branch pipeline is configured
-        And builds for "build_bar" and "build_deploy" jobs are part of one single event.
+      Scenario: Fan-out. Multiple external builds are triggered in parallel as a result of a build's success.
+          Given an existing pipeline on branch "pipelineA" with job "parallel_A"
+          And an existing pipeline on branch "pipelineB" with the workflow jobs:
+              | job             | requires      |
+              | parallel_B1 | ~sd@?:parallel_A |
+              | parallel_B2 | ~sd@?:parallel_A |
+          When the "parallel_A" job on branch "pipelineA" is started
+          And the "parallel_A" build succeeded
+          Then the "parallel_B1" job on branch "pipelineB" is started
+          And the "parallel_B1" build's parentBuildId is that "parallel_A" build's buildId
+          And the "parallel_B2" job on branch "pipelineB" is started
+          And the "parallel_B2" build's parentBuildId is that "parallel_A" build's buildId
