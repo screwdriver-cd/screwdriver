d9a53c6c993f87f1201a25cd0c6c445792ba7cd3
Tiffany K <tiffanykyi@gmail.com>
2020-07-28 14:43:40 -0700

    feat(2152): Support coverage scope param (#2161)

--- plugins/coverage/info.js
+++ plugins/coverage/info.js
@@ -19,10 +19,46 @@ module.exports = config => ({
             }
         },
         handler: (request, reply) => {
-            config.coveragePlugin
-                .getInfo(request.query)
-                .then(reply)
-                .catch(err => reply(boom.boomify(err)));
+            const { jobId, pipelineId, startTime, endTime, jobName, pipelineName, scope, projectKey } = request.query;
+            const { jobFactory } = request.server.app;
+            const infoConfig = { jobId, pipelineId, startTime, endTime, jobName, pipelineName };
+
+            // Short circuit to get coverage info
+            if (projectKey && startTime && endTime) {
+                return config.coveragePlugin
+                    .getInfo({ startTime, endTime, coverageProjectKey: projectKey })
+                    .then(reply)
+                    .catch(err => reply(boom.boomify(err)));
+            }
+
+            return (
+                Promise.resolve()
+                    // Get scope
+                    .then(() => {
+                        if (scope) {
+                            return { 'screwdriver.cd/coverageScope': request.query.scope };
+                        }
+                        if (!scope && jobId) {
+                            return jobFactory.get(jobId).then(job => {
+                                if (!job) {
+                                    throw boom.notFound('Job does not exist');
+                                }
+
+                                return job.permutations[0].annotations;
+                            });
+                        }
+
+                        return {};
+                    })
+                    .then(annotations => {
+                        infoConfig.annotations = annotations;
+
+                        return config.coveragePlugin
+                            .getInfo(infoConfig)
+                            .then(reply)
+                            .catch(err => reply(boom.boomify(err)));
+                    })
+            );
         }
     }
 });
