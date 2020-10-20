e698e39916dc84922f3c0e21563cf10c4a379669
Pritam Paul <pritam.geek09@gmail.com>
2020-10-20 14:31:49 -0400

    feat: add provision to set canary cookie (#2254)
    * feat: add provision to set canary cookie
    
    * feat: remove mode check and rely on config
    
    * feat: reduce cookie ttl
    
    * fix linter error
    
    * add default values

--- test/lib/server.test.js
+++ test/lib/server.test.js
@@ -23,12 +23,17 @@ describe('server case', () => {
             executor: {}
         },
         auth: {
+            sessionTimeout: 2,
             scm: {
                 getBellConfiguration: sinon.stub().resolves()
             }
         },
         httpd: {
             port: 12347
+        },
+        release: {
+            cookieName: 'test_cookie',
+            cookieValue: 'test_value'
         }
     };
 
