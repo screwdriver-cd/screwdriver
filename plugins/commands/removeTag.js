5bb63c77819e4cd7e904d30df71004ac306559e6
Ibuki <ibu1224@users.noreply.github.com>
2021-04-06 23:51:10 +0900

    feat(2399): Remove command tag (#2400)

---
+++ plugins/commands/removeTag.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const boom = require('@hapi/boom');
+const joi = require('joi');
+const schema = require('screwdriver-data-schema');
+const baseSchema = schema.models.commandTag.base;
+/* Currently, only build scope is allowed to tag command due to security reasons.
+ * The same pipeline that publishes the commands has the permission to tag it.
+ */
+
+module.exports = () => ({
+    method: 'DELETE',
+    path: '/commands/{namespace}/{name}/tags/{tagName}',
+    options: {
+        description: 'Delete a command tag',
+        notes: 'Delete a specific command tag',
+        tags: ['api', 'commands'],
+        auth: {
+            strategies: ['token'],
+            scope: ['build']
+        },
+        handler: async (request, h) => {
+            const { pipelineFactory, commandFactory, commandTagFactory } = request.server.app;
+            const { pipelineId, isPR } = request.auth.credentials;
+            const { namespace, name } = request.params;
+            const tag = request.params.tagName;
+
+            return commandTagFactory
+                .get({ namespace, name, tag })
+                .then(commandTag => {
+                    if (!commandTag) {
+                        throw boom.notFound(`Command tag ${tag} does not exist for ${namespace}/${name}`);
+                    }
+
+                    return Promise.all([
+                        pipelineFactory.get(pipelineId),
+                        commandFactory.get({
+                            name,
+                            namespace,
+                            version: commandTag.version
+                        })
+                    ]).then(([pipeline, command]) => {
+                        // Check for permission
+                        if (pipeline.id !== command.pipelineId || isPR) {
+                            throw boom.forbidden('Not allowed to delete this command tag');
+                        }
+
+                        // Remove the command tag, not the command
+                        return commandTag.remove();
+                    });
+                })
+                .then(() => h.response().code(204))
+                .catch(err => {
+                    throw err;
+                });
+        },
+        validate: {
+            params: joi.object({
+                namespace: baseSchema.extract('namespace'),
+                name: baseSchema.extract('name'),
+                tagName: baseSchema.extract('tag')
+            })
+        }
+    }
+});
