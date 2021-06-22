d8bd3979fa573ed2cebae7b99432e71b0df62a93
Jithin Emmanuel <jithine@verizonmedia.com>
2021-06-21 17:46:05 -0700

    fix: refactor to address eslint warnings (#2485)
    * fix: remove warning relaxation
    
    * remove quiet
    
    * fix: limit max warnings to 100
    disable complexity for test file
    refactory code base to address complexity rule
    
    * fix: refactory build/update to async/await

--- plugins/builds/update.js
+++ plugins/builds/update.js
@@ -8,7 +8,8 @@ const idSchema = schema.models.job.base.extract('id');
 const { getScmUri, getUserPermissions } = require('../helper');
 
 /**
- * Determine if this build is FIXED build or not.
+ * Identify whether this build resulted in a previously failed job to become successful.
+ *
  * @method isFixedBuild
  * @param  build         Build Object
  * @param  jobFactory    Job Factory instance
@@ -22,13 +23,7 @@ async function isFixedBuild(build, jobFactory) {
     const failureBuild = await job.getLatestBuild({ status: 'FAILURE' });
     const successBuild = await job.getLatestBuild({ status: 'SUCCESS' });
 
-    if (!failureBuild) {
-        return false;
-    }
-    if (failureBuild && !successBuild) {
-        return true;
-    }
-    if (failureBuild.id > successBuild.id) {
+    if ((failureBuild && !successBuild) || failureBuild.id > successBuild.id) {
         return true;
     }
 
@@ -49,6 +44,115 @@ async function stopFrozenBuild(build, previousStatus) {
     return build.stopFrozen(previousStatus);
 }
 
+/**
+ * Updates execution details for init step
+ * @method stopFrozenBuild
+ * @param  {Object} build       Build Object
+ * @param  {Object} app         Hapi app Object
+ */
+async function updateInitStep(build, app) {
+    const step = await app.stepFactory.get({ buildId: build.id, name: 'sd-setup-init' });
+    // If there is no init step, do nothing
+
+    if (!step) {
+        return null;
+    }
+
+    step.endTime = build.startTime || new Date().toISOString();
+    step.code = 0;
+
+    return step.update();
+}
+
+/**
+ * Validate if build status can be updated
+ * @method validateBuildStatus
+ * @param  {String} id            Build Id
+ * @param  {Object} buildFactory  Build factory object to quey build store
+ */
+async function getBuildToUpdate(id, buildFactory) {
+    const build = await buildFactory.get(id);
+
+    if (!build) {
+        throw boom.notFound(`Build ${id} does not exist`);
+    }
+
+    // Check build status
+    if (!['RUNNING', 'QUEUED', 'BLOCKED', 'UNSTABLE', 'FROZEN'].includes(build.status)) {
+        throw boom.forbidden('Can only update RUNNING, QUEUED, BLOCKED, FROZEN, or UNSTABLE builds');
+    }
+
+    return build;
+}
+
+/**
+ *
+ * @param  {Object} build Build object
+ * @param  {Object} request hapi request object
+ * @throws boom.badRequest on validation error
+ */
+async function validateUserPermission(build, request) {
+    const { jobFactory, userFactory, bannerFactory, pipelineFactory } = request.server.app;
+    const { username, scmContext } = request.auth.credentials;
+
+    const { status: desiredStatus } = request.payload;
+
+    const scmDisplayName = bannerFactory.scm.getDisplayName({ scmContext });
+    // Check if Screwdriver admin
+    const adminDetails = request.server.plugins.banners.screwdriverAdminDetails(username, scmDisplayName);
+
+    // Check desired status
+    if (adminDetails.isAdmin) {
+        if (desiredStatus !== 'ABORTED' && desiredStatus !== 'FAILURE') {
+            throw boom.badRequest('Admin can only update builds to ABORTED or FAILURE');
+        }
+    } else if (desiredStatus !== 'ABORTED') {
+        throw boom.badRequest('User can only update builds to ABORTED');
+    }
+
+    // Check permission against the pipeline
+    // Fetch the job and user models
+    const [job, user] = await Promise.all([jobFactory.get(build.jobId), userFactory.get({ username, scmContext })]);
+
+    const pipeline = await job.pipeline;
+
+    // Use parent's scmUri if pipeline is child pipeline and using read-only SCM
+    const scmUri = await getScmUri({ pipeline, pipelineFactory });
+
+    // Check the user's permission
+    await getUserPermissions({ user, scmUri, level: 'push', isAdmin: adminDetails.isAdmin });
+}
+
+/**
+ *
+ * @param {Object} build         Build Model
+ * @param {String} desiredStatus New Status
+ * @param {String} statusMessage User passed status message
+ * @param {String} username      User initiating status build update
+ */
+function updateBuildStatus(build, desiredStatus, statusMessage, username) {
+    // UNSTABLE -> SUCCESS needs to update meta and endtime.
+    // However, the status itself cannot be updated to SUCCESS
+    const currentStatus = build.status;
+
+    if (currentStatus !== 'UNSTABLE') {
+        build.status = desiredStatus;
+        if (build.status === 'ABORTED') {
+            if (currentStatus === 'FROZEN') {
+                build.statusMessage = `Frozen build aborted by ${username}`;
+            } else {
+                build.statusMessage = `Aborted by ${username}`;
+            }
+        } else if (build.status === 'FAILURE' || build.status === 'SUCCESS') {
+            if (statusMessage) {
+                build.statusMessage = statusMessage;
+            }
+        } else {
+            build.statusMessage = statusMessage || null;
+        }
+    }
+}
+
 module.exports = () => ({
     method: 'PUT',
     path: '/builds/{id}',
@@ -62,16 +166,7 @@ module.exports = () => ({
         },
 
         handler: async (request, h) => {
-            // eslint-disable-next-line max-len
-            const {
-                buildFactory,
-                eventFactory,
-                jobFactory,
-                userFactory,
-                stepFactory,
-                bannerFactory,
-                pipelineFactory
-            } = request.server.app;
+            const { buildFactory, eventFactory, jobFactory } = request.server.app;
             const { id } = request.params;
             const { statusMessage, stats, status: desiredStatus } = request.payload;
             const { username, scmContext, scope } = request.auth.credentials;
@@ -82,197 +177,80 @@ module.exports = () => ({
                 return boom.forbidden(`Credential only valid for ${username}`);
             }
 
-            return buildFactory
-                .get(id)
-                .then(async build => {
-                    if (!build) {
-                        throw boom.notFound(`Build ${id} does not exist`);
-                    }
+            const build = await getBuildToUpdate(id, buildFactory);
 
-                    // Check build status
-                    if (!['RUNNING', 'QUEUED', 'BLOCKED', 'UNSTABLE', 'FROZEN'].includes(build.status)) {
-                        throw boom.forbidden('Can only update RUNNING, QUEUED, BLOCKED, FROZEN, or UNSTABLE builds');
-                    }
+            if (!isBuild) {
+                await validateUserPermission(build, request);
+            }
+            const event = await eventFactory.get(build.eventId);
 
-                    // Users can only mark a running or queued build as aborted
-                    if (!isBuild) {
-                        const scmDisplayName = bannerFactory.scm.getDisplayName({ scmContext });
-                        // Check if Screwdriver admin
-                        const adminDetails = request.server.plugins.banners.screwdriverAdminDetails(
-                            username,
-                            scmDisplayName
-                        );
+            if (stats) {
+                // need to do this so the field is dirty
+                build.stats = Object.assign(build.stats, stats);
+            }
 
-                        // Check desired status
-                        if (adminDetails.isAdmin) {
-                            if (desiredStatus !== 'ABORTED' && desiredStatus !== 'FAILURE') {
-                                throw boom.badRequest('Admin can only update builds to ABORTED or FAILURE');
-                            }
-                        } else if (desiredStatus !== 'ABORTED') {
-                            throw boom.badRequest('User can only update builds to ABORTED');
-                        }
+            // Short circuit for cases that don't need to update status
+            if (!desiredStatus) {
+                build.statusMessage = statusMessage || build.statusMessage;
+            } else if (['SUCCESS', 'FAILURE', 'ABORTED'].includes(desiredStatus)) {
+                build.meta = request.payload.meta || {};
+                event.meta = { ...event.meta, ...build.meta };
+                build.endTime = new Date().toISOString();
+            } else if (desiredStatus === 'RUNNING') {
+                build.startTime = new Date().toISOString();
+            } else if (desiredStatus === 'BLOCKED' && !hoek.reach(build, 'stats.blockedStartTime')) {
+                build.stats = Object.assign(build.stats, {
+                    blockedStartTime: new Date().toISOString()
+                });
+            } else if (desiredStatus === 'QUEUED') {
+                throw boom.badRequest(`Cannot update builds to ${desiredStatus}`);
+            }
 
-                        // Fetch the job and user models
-                        const [job, user] = await Promise.all([
-                            jobFactory.get(build.jobId),
-                            userFactory.get({ username, scmContext })
-                        ]);
+            const currentStatus = build.status;
+            let isFixed = Promise.resolve(false);
+            let stopFrozen = null;
 
-                        const pipeline = await job.pipeline;
+            updateBuildStatus(build, desiredStatus, statusMessage, username);
 
-                        // Use parent's scmUri if pipeline is child pipeline and using read-only SCM
-                        const scmUri = await getScmUri({ pipeline, pipelineFactory });
+            // If status got updated to RUNNING or COLLAPSED, update init endTime and code
+            if (['RUNNING', 'COLLAPSED', 'FROZEN'].includes(desiredStatus)) {
+                await updateInitStep(build, request.server.app);
+            } else {
+                stopFrozen = stopFrozenBuild(build, currentStatus);
+                isFixed = isFixedBuild(build, jobFactory);
+            }
 
-                        // Check the user's permission
-                        await getUserPermissions({ user, scmUri, level: 'push', isAdmin: adminDetails.isAdmin });
-                    }
+            const [newBuild, newEvent] = await Promise.all([build.update(), event.update(), stopFrozen]);
 
-                    return eventFactory.get(build.eventId).then(event => ({ build, event }));
-                })
-                .then(async ({ build, event }) => {
-                    // We can't merge from executor-k8s/k8s-vm side because executor doesn't have build object
-                    // So we do merge logic here instead
+            const job = await newBuild.job;
+            const pipeline = await job.pipeline;
 
-                    if (stats) {
-                        // need to do this so the field is dirty
-                        build.stats = Object.assign(build.stats, stats);
-                    }
+            await request.server.events.emit('build_status', {
+                settings: job.permutations[0].settings,
+                status: newBuild.status,
+                event: newEvent.toJson(),
+                pipeline: pipeline.toJson(),
+                jobName: job.name,
+                build: newBuild.toJson(),
+                buildLink: `${buildFactory.uiUri}/pipelines/${pipeline.id}/builds/${id}`,
+                isFixed: await isFixed
+            });
 
-                    // Short circuit for cases that don't need to update status
-                    if (!desiredStatus) {
-                        if (statusMessage) {
-                            build.statusMessage = statusMessage;
-                        }
+            const skipFurther = /\[(skip further)\]/.test(newEvent.causeMessage);
 
-                        return Promise.all([build.update(), event.update()]);
-                    }
+            // Guard against triggering non-successful or unstable builds
+            // Don't further trigger pipeline if intented to skip further jobs
 
-                    switch (desiredStatus) {
-                        case 'SUCCESS':
-                        case 'FAILURE':
-                        case 'ABORTED':
-                            build.meta = request.payload.meta || {};
-                            event.meta = { ...event.meta, ...build.meta };
-                            build.endTime = new Date().toISOString();
-                            break;
-                        case 'RUNNING':
-                            build.startTime = new Date().toISOString();
-                            break;
-                        // do not update meta or endTime for these cases
-                        case 'UNSTABLE':
-                            break;
-                        case 'BLOCKED':
-                            if (!hoek.reach(build, 'stats.blockedStartTime')) {
-                                build.stats = Object.assign(build.stats, {
-                                    blockedStartTime: new Date().toISOString()
-                                });
-                            }
+            if (newBuild.status !== 'SUCCESS' || skipFurther) {
+                // Check for failed jobs and remove any child jobs in created state
+                if (newBuild.status === 'FAILURE') {
+                    await removeJoinBuilds({ pipeline, job, build: newBuild }, request.server.app);
+                }
+            } else {
+                await triggerNextJobs({ pipeline, job, build: newBuild, username, scmContext }, request.server.app);
+            }
 
-                            break;
-                        case 'FROZEN':
-                        case 'COLLAPSED':
-                            break;
-                        default:
-                            throw boom.badRequest(`Cannot update builds to ${desiredStatus}`);
-                    }
-
-                    const currentStatus = build.status;
-                    // UNSTABLE -> SUCCESS needs to update meta and endtime.
-                    // However, the status itself cannot be updated to SUCCESS
-
-                    if (currentStatus !== 'UNSTABLE') {
-                        build.status = desiredStatus;
-                        if (build.status === 'ABORTED') {
-                            if (currentStatus === 'FROZEN') {
-                                build.statusMessage = `Frozen build aborted by ${username}`;
-                            } else {
-                                build.statusMessage = `Aborted by ${username}`;
-                            }
-                        } else if (build.status === 'FAILURE' || build.status === 'SUCCESS') {
-                            if (statusMessage) {
-                                build.statusMessage = statusMessage;
-                            }
-                        } else {
-                            build.statusMessage = statusMessage || null;
-                        }
-                    }
-
-                    // If status got updated to RUNNING or COLLAPSED, update init endTime and code
-                    if (['RUNNING', 'COLLAPSED', 'FROZEN'].includes(desiredStatus)) {
-                        const step = await stepFactory.get({ buildId: id, name: 'sd-setup-init' });
-
-                        // If there is no init step, do nothing
-                        if (step) {
-                            step.endTime = build.startTime || new Date().toISOString();
-                            step.code = 0;
-
-                            await step.update();
-                        }
-
-                        return Promise.all([build.update(), event.update()]);
-                    }
-
-                    // Only trigger next build on success
-                    return Promise.all([
-                        build.update(),
-                        event.update(),
-                        isFixedBuild(build, jobFactory),
-                        stopFrozenBuild(build, currentStatus)
-                    ]);
-                })
-                .then(([newBuild, newEvent, isFixed]) =>
-                    newBuild.job.then(job =>
-                        job.pipeline
-                            .then(async pipeline => {
-                                await request.server.events.emit('build_status', {
-                                    settings: job.permutations[0].settings,
-                                    status: newBuild.status,
-                                    event: newEvent.toJson(),
-                                    pipeline: pipeline.toJson(),
-                                    jobName: job.name,
-                                    build: newBuild.toJson(),
-                                    buildLink: `${buildFactory.uiUri}/pipelines/${pipeline.id}/builds/${id}`,
-                                    isFixed: isFixed || false
-                                });
-
-                                const skipFurther = /\[(skip further)\]/.test(newEvent.causeMessage);
-
-                                // Guard against triggering non-successful or unstable builds
-                                // Don't further trigger pipeline if intented to skip further jobs
-                                if (newBuild.status !== 'SUCCESS' || skipFurther) {
-                                    // Check for failed jobs and remove any child jobs in created state
-                                    if (newBuild.status === 'FAILURE') {
-                                        await removeJoinBuilds(
-                                            {
-                                                pipeline,
-                                                job,
-                                                build: newBuild
-                                            },
-                                            request.server.app
-                                        );
-                                    }
-
-                                    return h.response(await newBuild.toJsonWithSteps()).code(200);
-                                }
-
-                                return triggerNextJobs(
-                                    {
-                                        pipeline,
-                                        job,
-                                        build: newBuild,
-                                        username,
-                                        scmContext
-                                    },
-                                    request.server.app
-                                ).then(async () => {
-                                    return h.response(await newBuild.toJsonWithSteps()).code(200);
-                                });
-                            })
-                            .catch(err => {
-                                throw err;
-                            })
-                    )
-                );
+            return h.response(await newBuild.toJsonWithSteps()).code(200);
         },
         validate: {
             params: joi.object({
