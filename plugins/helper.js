6ccdfbcf98779eeb0df76e813da5e6c66d353f37
klu909 <55161078+klu909@users.noreply.github.com>
2021-08-13 10:16:01 -0700

    fix: return property header for artifact endpoint (#2528)
    * fix: return property header for artifact endpoint
    
    * remove logic

--- plugins/helper.js
+++ plugins/helper.js
@@ -2,53 +2,6 @@
 
 const boom = require('@hapi/boom');
 const dayjs = require('dayjs');
-const mime = require('mime-types');
-
-const KNOWN_FILE_EXTS_IN_TEXT_FORMAT = [
-    'js',
-    'c',
-    'cpp',
-    'cs',
-    'dtd',
-    'h',
-    'm',
-    'java',
-    'lua',
-    'pl',
-    'go',
-    'py',
-    'tox',
-    'env',
-    'sh',
-    'vb',
-    'swift',
-    'yidf',
-    'state',
-    'diff',
-    'deps',
-    'xml'
-];
-
-/**
- * Get MIME types from file name and file extension
- * @method getMimeFromFileName
- * @param  {String} fileExtension  File extension (e.g. css, txt, html)
- * @param  {String} fileName       File name      (e.g. dockerfile, main)
- * @return {String} MIME Type      eg. text/html, text/plain
- */
-function getMimeFromFileName(fileExtension, fileName = '') {
-    if (fileName.toLowerCase().endsWith('file')) {
-        return 'text/plain';
-    }
-
-    if (KNOWN_FILE_EXTS_IN_TEXT_FORMAT.includes(fileExtension.toLowerCase())) {
-        return 'text/plain';
-    }
-
-    return mime.lookup(fileExtension) || '';
-}
-
-const displayableMimes = ['text/html'];
 
 /**
  * Set default start time and end time
@@ -158,7 +111,5 @@ module.exports = {
     getScmUri,
     getUserPermissions,
     setDefaultTimeRange,
-    validTimeRange,
-    getMimeFromFileName,
-    displayableMimes
+    validTimeRange
 };
