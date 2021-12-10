3eb5321f2d313062a35097f96f56b7e7cac8eb09
Jithin Emmanuel <jithine@verizonmedia.com>
2021-12-10 12:02:38 -0800

    fix: add new pipeline setting to test data (#2609)

--- test/plugins/pipelines.test.js
+++ test/plugins/pipelines.test.js
@@ -1933,6 +1933,7 @@ describe('pipeline plugin test', () => {
 
         it('returns 200 and updates settings only', () => {
             const expectedSetting = {
+                groupedEvents: false,
                 metricsDowntimeJobs: [123, 456],
                 public: true
             };
