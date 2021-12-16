1fdfe8aa4d9b16fea2fcbd02d9f24fcae719712e
s-yoshika <y.shota00@gmail.com>
2021-12-16 18:41:14 +0900

    fix(1500): add a new endpoint for POST /builds/{id}/artifacts/unzip

--- plugins/builds/index.js
+++ plugins/builds/index.js
@@ -11,6 +11,7 @@ const createRoute = require('./create');
 const stepGetRoute = require('./steps/get');
 const listStepsRoute = require('./steps/list');
 const artifactGetRoute = require('./artifacts/get');
+const artifactUnzipRoute = require('./artifacts/unzip');
 const stepUpdateRoute = require('./steps/update');
 const stepLogsRoute = require('./steps/logs');
 const listSecretsRoute = require('./listSecrets');
@@ -1106,7 +1107,8 @@ const buildsPlugin = {
             listSecretsRoute(),
             tokenRoute(),
             metricsRoute(),
-            artifactGetRoute(options)
+            artifactGetRoute(options),
+            artifactUnzipRoute(),
         ]);
     }
 };
