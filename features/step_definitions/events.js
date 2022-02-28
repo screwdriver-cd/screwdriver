bb8fe059158422eb09a3338377b8c65996a2605c
s-yoshika <y.shota00@gmail.com>
2022-02-28 17:47:20 +0900

    fix(1393) add a scenario into event.feature for the case of parent event

--- features/step_definitions/events.js
+++ features/step_definitions/events.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const Assert = require('chai').assert;
-const { Before, Given, Then } = require('cucumber');
+const { Before, Given, When, Then } = require('cucumber');
 const request = require('screwdriver-request');
+const sdapi = require('../support/sdapi');
 
 const TIMEOUT = 240 * 1000;
 
@@ -12,6 +13,7 @@ Before('@events', function hook() {
     // Reset shared information
     this.pipelineId = null;
     this.eventId = null;
+    this.previousEventId = null;
     this.jwt = null;
 });
 
@@ -21,6 +23,65 @@ Given(/^an existing pipeline with the workflow:$/, { timeout: TIMEOUT }, functio
 
 Given(/^"calvin" has admin permission to the pipeline$/, () => null);
 
+Given(/^the "main" job has a previous event$/, { timeout: TIMEOUT }, function step() {
+    const jobName = 'main';
+    return request({
+        url: `${this.instance}/${this.namespace}/events`,
+        method: 'POST',
+        json: {
+            pipelineId: this.pipelineId,
+            startFrom: jobName
+        },
+        context: {
+            token: this.jwt
+        }
+    })
+        .then(resp => {
+            Assert.equal(resp.statusCode, 201);
+            this.previousEventId = resp.body.id;
+        })
+        .then(() =>
+            sdapi.cleanupBuilds({
+                instance: this.instance,
+                pipelineId: this.pipelineId,
+                jobName,
+                jwt: this.jwt
+            })
+        );
+});
+
+When(/^the "main" job is restarted$/, { timeout: TIMEOUT }, function step() {
+    return request({
+        url: `${this.instance}/${this.namespace}/events`,
+        method: 'POST',
+        json: {
+            pipelineId: this.pipelineId,
+            parentEventId: this.previousEventId,
+            startFrom: 'main'
+        },
+        context: {
+            token: this.jwt
+        }
+    })
+        .then(response => {
+            Assert.equal(response.statusCode, 201);
+            this.eventId = response.body.id;
+        })
+        .then(() =>
+            request({
+                url: `${this.instance}/${this.namespace}/events/${this.eventId}/builds`,
+                method: 'GET',
+                context: {
+                    token: this.jwt
+                }
+            })
+        )
+        .then(response => {
+            Assert.equal(response.statusCode, 200);
+            this.buildId = response.body[0].id;
+        });
+});
+
 Then(/^an event is created$/, { timeout: TIMEOUT }, function step() {
     return request({
         url: `${this.instance}/${this.namespace}/pipelines/${this.pipelineId}/events`,
@@ -29,6 +90,19 @@ Then(/^an event is created$/, { timeout: TIMEOUT }, function step() {
             token: this.jwt
         }
     }).then(response => Assert.equal(response.body[0].id, this.eventId));
+});
+
+Then(/^an event is created with the parent event which is same as the previous event$/, { timeout: TIMEOUT }, function step() {
+    return request({
+        url: `${this.instance}/${this.namespace}/pipelines/${this.pipelineId}/events`,
+        method: 'GET',
+        context: {
+            token: this.jwt
+        }
+    }).then((response) => {
+        Assert.equal(response.body[0].id, this.eventId);
+        Assert.equal(response.body[0].parentEventId, this.previousEventId);
+    });
 });
 
 Then(/^the "main" build succeeds$/, { timeout: TIMEOUT }, function step() {
