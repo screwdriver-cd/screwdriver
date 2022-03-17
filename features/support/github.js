8e8c5ac39055c355a273575a0014881ca3ca2a81
Haruka Kawamura <itleigns@gmail.com>
2022-03-18 02:20:49 +0900

    feat: Add a functional test for skip build (#2685)

--- features/support/github.js
+++ features/support/github.js
@@ -139,21 +139,23 @@ function createBranch(branch, repoOwner, repoName) {
  * @param  {String}   [repoOwner]       Owner of the repository
  * @param  {String}   [repoName]        Name of the repository
  * @param  {String}   directoryName     Name of the directory
+ * @param  {String}   commitMessage     Commit message
  * @return {Promise}
  */
-function createFile(branch, repoOwner, repoName, directoryName) {
+function createFile(branch, repoOwner, repoName, directoryName, commitMessage) {
     // eslint-disable-next-line new-cap
     const content = new Buffer.alloc(MAX_CONTENT_LENGTH, randomString(MAX_CONTENT_LENGTH));
     const filename = randomString(MAX_FILENAME_LENGTH);
     const owner = repoOwner || 'screwdriver-cd-test';
     const repo = repoName || 'functional-git';
     const filePath = directoryName || 'testfiles';
+    const message = commitMessage || new Date().toString(); // default commit message is the current time
 
     return octokit.repos.createOrUpdateFileContents({
         owner,
         repo,
         path: `${filePath}/${filename}`,
-        message: new Date().toString(), // commit message is the current time
+        message,
         content: content.toString('base64'), // content needs to be transmitted in base64
         branch
     });
