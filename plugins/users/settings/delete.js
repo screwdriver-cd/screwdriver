e2f6fe8ef593afa8dcafe0c8d5f9d171f6c45b75
banusha2 <bollampally.anusha@yahooinc.com>
2022-08-31 15:11:37 -0400

    fix: remove payload from delete method

--- plugins/users/settings/delete.js
+++ plugins/users/settings/delete.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const boom = require('@hapi/boom');
-const schema = require('screwdriver-data-schema');
 
 module.exports = () => ({
     method: 'DELETE',
@@ -18,19 +17,13 @@ module.exports = () => ({
         handler: async (request, h) => {
             const { userFactory } = request.server.app;
             const { scmContext, username } = request.auth.credentials;
-            const { settings } = request.payload;
             const user = await userFactory.get({ username, scmContext });
 
             if (!user) {
                 throw boom.notFound('User does not exist');
             }
 
-            return user.updateSettings(settings).then(results => {
-                return h.response(results).code(200);
-            });
-        },
-        validate: {
-            payload: schema.models.user.update
+            return h.response('Uuser settings reset successful').code(204);
         }
     }
 });
