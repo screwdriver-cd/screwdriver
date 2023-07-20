d5555a5bd02a5bdc46745f6e9484b6bd3098f6f1
aishwaryanair04 <aishwaryanair.mec@gmail.com>
2023-07-20 15:43:19 -0500

    feat (2135): Added pipelineTemplateValidatorPlugin

--- plugins/template-validator.js
+++ plugins/template-validator.js
@@ -3,7 +3,8 @@
 const boom = require('@hapi/boom');
 const schema = require('screwdriver-data-schema');
 const templateSchema = schema.api.templateValidator;
-const validator = require('screwdriver-template-validator');
+const validator = require('screwdriver-template-validator').parseTemplate();
+const pipelineValidator = require('screwdriver-template-validator').parsePipelineTemplate();
 
 /**
  * Hapi Template Validator Plugin
@@ -51,4 +52,44 @@ const templateValidatorPlugin = {
     }
 };
 
-module.exports = templateValidatorPlugin;
+const pipelineTemplateValidatorPlugin = {
+    name: 'pipeline-template-validator',
+    async register(server) {
+        server.route({
+            method: 'POST',
+            path: 'pipeline/template/validate',
+            options: {
+                description: 'Validate a given sd-pipeline-template.yaml',
+                notes: 'returns the parsed config, validation errors, or both',
+                tags: ['api', 'validation', 'yaml'],
+                plugins: {
+                    'hapi-rate-limit': {
+                        enabled: false
+                    }
+                },
+                handler: async (request, h) => {
+                    try {
+                        const { yaml: templateString } = request.payload;
+
+                        const result = await pipelineValidator(templateString);
+
+                        return h.response(result);
+                    } catch (err) {
+                        throw boom.badRequest(err.toString());
+                    }
+                },
+                validate: {
+                    payload: templateSchema.input
+                },
+                response: {
+                    schema: templateSchema.output
+                }
+            }
+        });
+    }
+};
+
+module.exports = {
+    templateValidatorPlugin,
+    pipelineTemplateValidatorPlugin
+};
