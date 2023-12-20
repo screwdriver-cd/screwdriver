2959ab6cf054c5553633c0bfa81dd226057a4312
pritamstyz4ever <pritam.paul@yahooinc.com>
2023-12-19 19:03:02 -0500

    feat: get, createTag, listTags APIs

--- test/plugins/pipelines.templates.test.js
+++ test/plugins/pipelines.templates.test.js
@@ -5,9 +5,13 @@ const badgeMaker = require('badge-maker');
 const sinon = require('sinon');
 const hapi = require('@hapi/hapi');
 const hoek = require('@hapi/hoek');
+const testPipeline = require('./data/pipeline.json');
 const testTemplate = require('./data/pipeline-template.json');
 const testTemplates = require('./data/pipelineTemplates.json');
+const testTemplateGet = testTemplates[0];
 const testTemplateVersions = require('./data/pipelineTemplateVersions.json');
+const testTemplateVersionsGet = require('./data/pipelineTemplateVersion.json');
+const testTemplateTags = require('./data/pipelineTemplateTags.json');
 const TEMPLATE_INVALID = require('./data/pipeline-template-validator.missing-version.json');
 const TEMPLATE_VALID = require('./data/pipeline-template-validator.input.json');
 const TEMPLATE_VALID_NEW_VERSION = require('./data/pipeline-template-create.input.json');
@@ -30,6 +34,14 @@ const getTemplateMocks = templates => {
     return decorateObj(templates);
 };
 
+const getPipelineMocks = pipelines => {
+    if (Array.isArray(pipelines)) {
+        return pipelines.map(decorateObj);
+    }
+
+    return decorateObj(pipelines);
+};
+
 describe('pipeline plugin test', () => {
     let pipelineFactoryMock;
     let userFactoryMock;
@@ -46,6 +58,7 @@ describe('pipeline plugin test', () => {
     let scmMock;
     let pipelineTemplateFactoryMock;
     let pipelineTemplateVersionFactoryMock;
+    let pipelineTemplateTagFactoryMock;
     let plugin;
     let server;
     const password = 'this_is_a_password_that_needs_to_be_atleast_32_characters';
@@ -116,12 +129,20 @@ describe('pipeline plugin test', () => {
         screwdriverAdminDetailsMock = sinon.stub();
         pipelineTemplateFactoryMock = {
             get: sinon.stub(),
-            list: sinon.stub()
+            list: sinon.stub(),
+            create: sinon.stub()
         };
         pipelineTemplateVersionFactoryMock = {
             create: sinon.stub(),
             list: sinon.stub(),
-            get: sinon.stub()
+            get: sinon.stub(),
+            getWithMetadata: sinon.stub()
+        };
+
+        pipelineTemplateTagFactoryMock = {
+            list: sinon.stub(),
+            get: sinon.stub(),
+            create: sinon.stub()
         };
 
         /* eslint-disable global-require */
@@ -143,6 +164,7 @@ describe('pipeline plugin test', () => {
             secretFactory: secretFactoryMock,
             pipelineTemplateFactory: pipelineTemplateFactoryMock,
             pipelineTemplateVersionFactory: pipelineTemplateVersionFactoryMock,
+            pipelineTemplateTagFactory: pipelineTemplateTagFactoryMock,
             ecosystem: {
                 badges: '{{subject}}/{{status}}/{{color}}'
             }
@@ -541,6 +563,326 @@ describe('pipeline plugin test', () => {
 
             return server.inject(options).then(reply => {
                 assert.equal(reply.statusCode, 500);
+            });
+        });
+    });
+
+    describe('GET /pipeline/templates/{namespace}/{name}/tags', () => {
+        let options;
+
+        beforeEach(() => {
+            options = {
+                method: 'GET',
+                url: '/pipeline/templates/screwdriver/nodejs/tags',
+                auth: {
+                    credentials: {
+                        username: 'foo',
+                        scmContext: 'github:github.com',
+                        scope: ['user']
+                    },
+                    strategy: ['token']
+                }
+            };
+
+            pipelineTemplateTagFactoryMock.list.resolves(testTemplateTags);
+        });
+
+        it('returns 200 for getting all template tags for name and namespace', () =>
+            server.inject(options).then(reply => {
+                assert.deepEqual(reply.result, testTemplateTags);
+                assert.equal(reply.statusCode, 200);
+            }));
+
+        it('returns 200 and all tags for a pipeline template name and namespace with pagination', () => {
+            options.url = '/pipeline/templates/screwdriver/nodejs/tags?count=30';
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 200);
+                assert.deepEqual(reply.result, testTemplateTags);
+                assert.calledWith(pipelineTemplateTagFactoryMock.list, {
+                    params: {
+                        name: 'nodejs',
+                        namespace: 'screwdriver'
+                    },
+                    paginate: {
+                        page: undefined,
+                        count: 30
+                    },
+                    sort: 'descending'
+                });
+            });
+        });
+
+        it('returns 200 and an empty array when there are no template tags', () => {
+            pipelineTemplateTagFactoryMock.list.resolves([]);
+
+            return server.inject(options).then(reply => {
+                assert.deepEqual(reply.result, []);
+                assert.equal(reply.statusCode, 200);
+            });
+        });
+
+        it('returns 500 when the pipeline template model fails to get', () => {
+            const testError = new Error('templateModelGetError');
+
+            pipelineTemplateTagFactoryMock.list.rejects(testError);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 500);
+            });
+        });
+    });
+
+    describe('GET /pipeline/template/{namespace}/{name}', () => {
+        let options;
+
+        beforeEach(() => {
+            options = {
+                method: 'GET',
+                url: '/pipeline/template/screwdriver/nodejs',
+                auth: {
+                    credentials: {
+                        username: 'foo',
+                        scmContext: 'github:github.com',
+                        scope: ['user']
+                    },
+                    strategy: ['token']
+                }
+            };
+
+            pipelineTemplateFactoryMock.get.resolves(testTemplateGet);
+        });
+
+        it('returns 200 for getting a pipeline template', () =>
+            server.inject(options).then(reply => {
+                assert.deepEqual(reply.result, testTemplateGet);
+                assert.equal(reply.statusCode, 200);
+            }));
+
+        it('returns 404 when pipeline template does not exist', () => {
+            pipelineTemplateFactoryMock.get.resolves(null);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 404);
+            });
+        });
+
+        it('returns 500 when the pipeline template model fails to get', () => {
+            const testError = new Error('templateModelGetError');
+
+            pipelineTemplateFactoryMock.get.rejects(testError);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 500);
+            });
+        });
+    });
+
+    describe('GET /pipeline/template/{id}', () => {
+        let options;
+
+        beforeEach(() => {
+            options = {
+                method: 'GET',
+                url: '/pipeline/template/123',
+                auth: {
+                    credentials: {
+                        username: 'foo',
+                        scmContext: 'github:github.com',
+                        scope: ['user']
+                    },
+                    strategy: ['token']
+                }
+            };
+
+            pipelineTemplateFactoryMock.get.resolves(testTemplateGet);
+        });
+
+        it('returns 200 for getting a template', () =>
+            server.inject(options).then(reply => {
+                assert.deepEqual(reply.result, testTemplateGet);
+                assert.equal(reply.statusCode, 200);
+            }));
+
+        it('returns 404 when template does not exist', () => {
+            pipelineTemplateFactoryMock.get.resolves(null);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 404);
+            });
+        });
+
+        it('returns 500 when the template model fails to get', () => {
+            const testError = new Error('templateModelGetError');
+
+            pipelineTemplateFactoryMock.get.rejects(testError);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 500);
+            });
+        });
+    });
+
+    describe('GET /pipeline/template/{namespace}/{name}/{versionOrTag}', () => {
+        let options;
+
+        beforeEach(() => {
+            options = {
+                method: 'GET',
+                url: '/pipeline/template/screwdriver/nodejs/1.2.3',
+                auth: {
+                    credentials: {
+                        username: 'foo',
+                        scmContext: 'github:github.com',
+                        scope: ['user']
+                    },
+                    strategy: ['token']
+                }
+            };
+
+            pipelineTemplateVersionFactoryMock.getWithMetadata.resolves(testTemplateVersionsGet);
+        });
+
+        it('returns 200 for getting a template', () =>
+            server.inject(options).then(reply => {
+                assert.deepEqual(reply.result, testTemplateVersionsGet);
+                assert.equal(reply.statusCode, 200);
+            }));
+
+        it('returns 404 when template does not exist', () => {
+            pipelineTemplateVersionFactoryMock.getWithMetadata.resolves(null);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 404);
+            });
+        });
+
+        it('returns 500 when the template model fails to get', () => {
+            const testError = new Error('templateModelGetError');
+
+            pipelineTemplateVersionFactoryMock.getWithMetadata.rejects(testError);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 500);
+            });
+        });
+    });
+
+    describe('PUT /pipeline/template/{namespace}/{name}/tags/{tag}', () => {
+        let options;
+        let templateMock;
+        const testId = 123;
+        let pipeline;
+        const payload = {
+            version: '1.2.3'
+        };
+        const testTemplateTag = decorateObj(hoek.merge({ id: 123 }, payload));
+
+        beforeEach(() => {
+            options = {
+                method: 'PUT',
+                url: '/pipeline/template/screwdriver/nodejs/tags/stable',
+                payload: {
+                    version: '1.2.3'
+                },
+                auth: {
+                    credentials: {
+                        username: 'foo',
+                        scmContext: 'github:github.com',
+                        scope: ['user']
+                    },
+                    strategy: ['token']
+                }
+            };
+
+            pipeline = getPipelineMocks(testPipeline);
+            pipelineFactoryMock.get.resolves(pipeline);
+            templateMock = getTemplateMocks(testTemplate);
+            pipelineTemplateVersionFactoryMock.get.resolves(templateMock);
+        });
+
+        it('creates template tag if template tag does not exist yet', () => {
+            pipelineTemplateTagFactoryMock.create.resolves(testTemplateTag);
+
+            return server.inject(options).then(reply => {
+                const expectedLocation = new URL(
+                    `${options.url}/${testId}`,
+                    `${reply.request.server.info.protocol}://${reply.request.headers.host}`
+                ).toString();
+
+                assert.deepEqual(reply.result, hoek.merge({ id: 123 }, payload));
+                assert.strictEqual(reply.headers.location, expectedLocation);
+                assert.calledWith(pipelineTemplateVersionFactoryMock.get, {
+                    name: 'nodejs',
+                    namespace: 'screwdriver',
+                    version: '1.2.3'
+                });
+                assert.calledWith(pipelineTemplateTagFactoryMock.get, {
+                    name: 'nodejs',
+                    namespace: 'screwdriver',
+                    tag: 'stable'
+                });
+                assert.calledWith(pipelineTemplateTagFactoryMock.create, {
+                    namespace: 'screwdriver',
+                    name: 'nodejs',
+                    tag: 'stable',
+                    version: '1.2.3'
+                });
+                assert.equal(reply.statusCode, 201);
+            });
+        });
+
+        it('updates template version if the tag already exists', () => {
+            const template = hoek.merge(
+                {
+                    update: sinon.stub().resolves(testTemplateTag)
+                },
+                testTemplateTag
+            );
+
+            pipelineTemplateTagFactoryMock.get.resolves(template);
+
+            return server.inject(options).then(reply => {
+                assert.deepEqual(reply.result.version, template.version);
+                assert.calledWith(pipelineTemplateVersionFactoryMock.get, {
+                    name: 'nodejs',
+                    namespace: 'screwdriver',
+                    version: '1.2.3'
+                });
+                assert.calledWith(pipelineTemplateTagFactoryMock.get, {
+                    name: 'nodejs',
+                    namespace: 'screwdriver',
+                    tag: 'stable'
+                });
+                assert.calledOnce(template.update);
+                assert.notCalled(pipelineTemplateVersionFactoryMock.create);
+                assert.equal(reply.statusCode, 200);
+            });
+        });
+
+        it('returns 403 when pipelineId does not match', () => {
+            templateMock = getTemplateMocks(testTemplate);
+            templateMock.pipelineId = 321;
+            pipelineTemplateVersionFactoryMock.get.resolves(templateMock);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 403);
+            });
+        });
+
+        it('returns 404 when template does not exist', () => {
+            pipelineTemplateVersionFactoryMock.get.resolves(null);
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 404);
+            });
+        });
+
+        it('returns 403 if it is a PR build', () => {
+            options.auth.credentials.isPR = true;
+
+            return server.inject(options).then(reply => {
+                assert.equal(reply.statusCode, 403);
             });
         });
     });
