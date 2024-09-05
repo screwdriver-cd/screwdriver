db864afe18fa6652130ba737e5378b5bc09e318f
Dayanand Sagar <dsagar@yahooinc.com>
2024-09-05 13:18:10 -0700

    feat(3171): Fix tests

--- test/plugins/pipelines.templates.test.js
+++ test/plugins/pipelines.templates.test.js
@@ -280,11 +280,11 @@ describe('pipeline plugin test', () => {
                             sourcePaths: []
                         }
                     },
-                    parameters: {},
-                    workflowGraph: {
-                        nodes: [{ name: '~pr' }, { name: '~commit' }, { name: 'main' }],
-                        edges: []
-                    }
+                    parameters: {}
+                },
+                workflowGraph: {
+                    nodes: [{ name: '~pr' }, { name: '~commit' }, { name: 'main' }],
+                    edges: []
                 },
                 pipelineId: 123
             };
