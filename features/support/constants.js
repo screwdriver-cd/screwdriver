1c41922f609fcf42244d564ed100ca27b68c89d5
Yuki Okesaku <yuokesak@yahoo-corp.jp>
2026-02-04 09:33:57 +0900

    fix

--- features/support/constants.js
+++ features/support/constants.js
@@ -9,5 +9,5 @@ module.exports = {
     ID: /\d+/,
     TEST_TIMEOUT_DEFAULT: process.env.TEST_TIMEOUT_DEFAULT || 240 * 1000, // 240 sec
     TEST_TIMEOUT_WITH_BUILD: process.env.TEST_TIMEOUT_WITH_BUILD || 500 * 1000, // 500 sec
-    TEST_TIMEOUT_WITH_SCM: process.TEST_TIMEOUT_WITH_SCM || 700 * 1000 // 900 sec
+    TEST_TIMEOUT_WITH_SCM: process.TEST_TIMEOUT_WITH_SCM || 700 * 1000 // 700 sec
 };
