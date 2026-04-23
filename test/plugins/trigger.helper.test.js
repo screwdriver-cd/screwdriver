2c686ab9c3fd2649477fe987226d54c3d158e000
Nihal Sharma <sharmanihal96@gmail.com>
2026-04-23 12:45:24 +0000

    style: run prettier/eslint autofix on new buildsToRestartFilter test

--- test/plugins/trigger.helper.test.js
+++ test/plugins/trigger.helper.test.js
@@ -1530,21 +1530,18 @@ describe('buildsToRestartFilter function', () => {
         assert.deepEqual(result, []);
     });
 
-   it('should not throw when an existing build has parentBuildId null', () => {
+    it('should not throw when an existing build has parentBuildId null', () => {
         const joinPipeline = {
             jobs: {
                 jobA: { id: 1 }
             }
         };
-        const groupEventBuilds = [
-            { jobId: 1, status: Status.SUCCESS, parentBuildId: null, eventId: 100 }
-        ];
+        const groupEventBuilds = [{ jobId: 1, status: Status.SUCCESS, parentBuildId: null, eventId: 100 }];
         const currentEvent = { parentEventId: 99 };
         const currentBuild = { id: 3 };
         const result = buildsToRestartFilter(joinPipeline, groupEventBuilds, currentEvent, currentBuild);
-        assert.deepEqual(result, [
-            { jobId: 1, status: Status.SUCCESS, parentBuildId: null, eventId: 100 }
-        ]);
+
+        assert.deepEqual(result, [{ jobId: 1, status: Status.SUCCESS, parentBuildId: null, eventId: 100 }]);
     });
 });
 
