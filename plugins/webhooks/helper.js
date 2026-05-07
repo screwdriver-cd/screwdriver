0275731cd24536d70a11460cad691154c51a07fc
Yuta Kaneda <yukaneda@lycorp.co.jp>
2026-05-07 19:41:56 +0900

    feat: wrap webhook SCM handling in request cache context

--- plugins/webhooks/helper.js
+++ plugins/webhooks/helper.js
@@ -1413,77 +1413,80 @@ async function startHookEvent(request, h, webhookConfig) {
     const { type, hookId, username, scmContext, ref, checkoutUrl, action, prNum } = webhookConfig;
 
     parsedHookId = hookId;
+    webhookConfig.requestCache = new Map();
 
     try {
-        // skipping checks
-        if (/\[(skip ci|ci skip)\]/.test(webhookConfig.lastCommitMessage)) {
-            skipMessage = 'Skipping due to the commit message: [skip ci]';
-        }
-
-        // if skip ci then don't return
-        if (action !== 'tag' && action !== 'release' && ignoreUser && ignoreUser.length !== 0 && !skipMessage) {
-            const commitAuthors =
-                Array.isArray(webhookConfig.commitAuthors) && webhookConfig.commitAuthors.length !== 0
-                    ? webhookConfig.commitAuthors
-                    : [username];
-            const validCommitAuthors = commitAuthors.filter(author => !ignoreUser.includes(author));
-
-            if (!validCommitAuthors.length) {
-                message = `Skipping because user ${username} is ignored`;
-                request.log(['webhook', hookId], message);
-
-                return h.response({ message }).code(204);
+        return scm.withRequestCache(webhookConfig.requestCache, async () => {
+            // skipping checks
+            if (/\[(skip ci|ci skip)\]/.test(webhookConfig.lastCommitMessage)) {
+                skipMessage = 'Skipping due to the commit message: [skip ci]';
             }
-        }
 
-        const token = await obtainScmToken({
-            pluginOptions: webhookConfig.pluginOptions,
-            userFactory,
-            username,
-            scmContext,
-            scm
-        });
+            // if skip ci then don't return
+            if (action !== 'tag' && action !== 'release' && ignoreUser && ignoreUser.length !== 0 && !skipMessage) {
+                const commitAuthors =
+                    Array.isArray(webhookConfig.commitAuthors) && webhookConfig.commitAuthors.length !== 0
+                        ? webhookConfig.commitAuthors
+                        : [username];
+                const validCommitAuthors = commitAuthors.filter(author => !ignoreUser.includes(author));
 
-        if (action !== 'release' && action !== 'tag') {
-            let scmUri;
+                if (!validCommitAuthors.length) {
+                    message = `Skipping because user ${username} is ignored`;
+                    request.log(['webhook', hookId], message);
+
+                    return h.response({ message }).code(204);
+                }
+            }
+
+            const token = await obtainScmToken({
+                pluginOptions: webhookConfig.pluginOptions,
+                userFactory,
+                username,
+                scmContext,
+                scm
+            });
+
+            if (action !== 'release' && action !== 'tag') {
+                let scmUri;
+
+                if (type === 'pr') {
+                    scmUri = await scm.parseUrl({ checkoutUrl, token, scmContext });
+                }
+                webhookConfig.changedFiles = await scm.getChangedFiles({
+                    webhookConfig,
+                    type,
+                    token,
+                    scmContext,
+                    scmUri,
+                    prNum
+                });
+                request.log(['webhook', hookId], `Changed files are ${webhookConfig.changedFiles}`);
+            } else {
+                // The payload has no sha when webhook event is tag or release, so we need to get it.
+                try {
+                    webhookConfig.sha = await getCommitRefSha({
+                        scm,
+                        token,
+                        ref,
+                        refType: 'tags',
+                        checkoutUrl,
+                        scmContext
+                    });
+                } catch (err) {
+                    request.log(['webhook', hookId, 'getCommitRefSha'], err);
+
+                    // there is a possibility of scm.getCommitRefSha() is not implemented yet
+                    return h.response({ message }).code(204);
+                }
+            }
 
             if (type === 'pr') {
-                scmUri = await scm.parseUrl({ checkoutUrl, token, scmContext });
+                // disregard skip ci for pull request events
+                return pullRequestEvent(webhookConfig.pluginOptions, request, h, webhookConfig, token);
             }
-            webhookConfig.changedFiles = await scm.getChangedFiles({
-                webhookConfig,
-                type,
-                token,
-                scmContext,
-                scmUri,
-                prNum
-            });
-            request.log(['webhook', hookId], `Changed files are ${webhookConfig.changedFiles}`);
-        } else {
-            // The payload has no sha when webhook event is tag or release, so we need to get it.
-            try {
-                webhookConfig.sha = await getCommitRefSha({
-                    scm,
-                    token,
-                    ref,
-                    refType: 'tags',
-                    checkoutUrl,
-                    scmContext
-                });
-            } catch (err) {
-                request.log(['webhook', hookId, 'getCommitRefSha'], err);
 
-                // there is a possibility of scm.getCommitRefSha() is not implemented yet
-                return h.response({ message }).code(204);
-            }
-        }
-
-        if (type === 'pr') {
-            // disregard skip ci for pull request events
-            return pullRequestEvent(webhookConfig.pluginOptions, request, h, webhookConfig, token);
-        }
-
-        return pushEvent(request, h, webhookConfig, skipMessage, token);
+            return pushEvent(request, h, webhookConfig, skipMessage, token);
+        });
     } catch (err) {
         logger.error(`[${parsedHookId}]: ${err}`);
 
