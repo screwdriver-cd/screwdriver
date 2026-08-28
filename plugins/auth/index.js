3aa6a45efeef64b88d7edf3c48fa7dbc8253711e
Keisuke Kumada <kekumada@yahoo-corp.jp>
2026-08-29 08:28:56 +0900

    feat(3508): add API Token access audit logging (#3524)

--- plugins/auth/index.js
+++ plugins/auth/index.js
@@ -4,6 +4,7 @@ const boom = require('@hapi/boom');
 const joi = require('joi');
 const jwt = require('jsonwebtoken');
 const { v4: uuidv4 } = require('uuid');
+const logger = require('screwdriver-logger');
 const contextsRoute = require('./contexts');
 const crumbRoute = require('./crumb');
 const keyRoute = require('./key');
@@ -124,16 +125,19 @@ const authPlugin = {
             const { apiTokenId } = credentials.auth;
 
             if (!apiTokenId) {
+                logger.info(`[API Token Access] Invalid API Token.`);
                 throw boom.forbidden(`Your token is invalid`);
             }
 
             const token = await tokenFactory.get({ id: apiTokenId });
 
             if (!token) {
+                logger.info(`[API Token Access] Invalid API Token.`);
                 throw boom.forbidden(`Your token is invalid`);
             }
 
             if (token.expiresAt && new Date(token.expiresAt) < new Date()) {
+                logger.info(`[API Token Access][TokenId:${apiTokenId}] Expired Token Access.`);
                 throw boom.forbidden(`Your token is expired: token id ${apiTokenId}`);
             }
 
@@ -146,9 +150,16 @@ const authPlugin = {
                 throw boom.badImplementation(`Invalid required permission: "${requiredPermission}"`);
             }
 
+            const endpoint = request.route.path;
+
             if (!actualPermissionLevel || actualPermissionLevel < requiredPermissionLevel) {
+                logger.info(
+                    `[API Token Access][TokenId:${apiTokenId}][${endpoint}] Invalid permission Token Access. This endpoint requires "${requiredPermission}" permission. This token has "${actualPermission}"`
+                );
                 throw boom.forbidden(`This endpoint requires "${requiredPermission}" permission`);
             }
+
+            logger.info(`[API Token Access][TokenId:${apiTokenId}][${endpoint}] Success API Token access.`);
 
             return h.continue;
         });
